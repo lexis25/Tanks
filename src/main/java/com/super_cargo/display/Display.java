a230a81d22cfea0b71c0e64b189579dd1442dcba
lexis25 <tokallex@gmail.com>
2017-06-26 22:33:45 +0300

    render image

--- src/main/java/com/super_cargo/display/Display.java
+++ src/main/java/com/super_cargo/display/Display.java
@@ -2,6 +2,9 @@ package com.super_cargo.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public abstract class Display {
 
@@ -9,7 +12,16 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create(int width, int height, String title){
+    private static BufferedImage buffer;
+    private static int[] bufferedData;
+    private static Graphics bufferedGraphics;
+    private static int clearColor;
+
+    //temp
+    private static float delte = 0;
+    //temp end
+
+    public static void create(int width, int height, String title, int _clearColor){
         if(created){
             return;
         }else {
@@ -26,8 +38,27 @@ public abstract class Display {
             window.setLocationRelativeTo(null);
             window.setVisible(true);
 
+            buffer = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+            bufferedData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+            bufferedGraphics = buffer.getGraphics();
+            clearColor = _clearColor;
 
+            created = true;
         }
+    }
 
+    public static void clear(){
+        Arrays.fill(bufferedData,clearColor);
+    }
+
+    public static void render(){
+        bufferedGraphics.setColor(new Color(0xff0000ff));
+        bufferedGraphics.fillOval((int)(350 + Math.sin(delte) * 200),250,100,100);
+        delte += 0.02f;
+    }
+
+    public static void swapBuffers(){
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer, 0,0, null);
     }
 }
--- src/main/java/com/super_cargo/main/Main.java
+++ src/main/java/com/super_cargo/main/Main.java
@@ -2,9 +2,26 @@ package com.super_cargo.main;
 
 import com.super_cargo.display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Main {
 
     public static void main(String[] args) {
-        Display.create(800,600,"");
+        Display.create(800,600,"Tanks",0xff00ff00);
+
+         Timer t = new Timer (1000/60, new AbstractAction(){
+
+             public void actionPerformed(ActionEvent e) {
+                 Display.clear();
+                 Display.render();
+                 Display.swapBuffers();
+             }
+        });
+
+         t.setRepeats(true);
+         t.start();
+
+
     }
 }
